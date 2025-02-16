e967c67f3182f7062f974c1219a70ddb65ad51b7
robson88ce2 <106639177+robson88ce2@users.noreply.github.com>
2025-02-16 00:16:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ def get_db_connection():
         user=os.getenv("PG_USER", "postgres"),
         password=os.getenv("PG_PASS", "kvDwfuepWWYapBconiTHOmcxjesQIVIb"),
         host=os.getenv("PG_HOST", "turntable.proxy.rlwy.net:34145"),
-        port=os.getenv("PG_PORT", "5432")
+        port=os.getenv("PG_PORT", "5432"),
+        conect=${{ Postgres.DATABASE_URL }}
     )
 # Função para executar queries no banco de dados
 def execute_query(query, params=None, fetch=False):
